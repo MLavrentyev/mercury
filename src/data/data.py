13515fa9044e399662b1282876b342b983514622
Mark Lavrentyev <lavrema@outlook.com>
2020-07-18 17:26:54 -0400

    Add data receiving framework

---
+++ src/data/data.py
@@ -0,0 +1,12 @@
+
+
+class DataPoint:
+    def __init__(self, **kwargs):
+        self.coolantTemp = kwargs.get("coolantTemp")
+        self.oilPressure = kwargs.get("oilPressure")
+        self.batteryVoltage = kwargs.get("batteryVoltage")
+        self.lambdaValue = kwargs.get("lambdaValue")
+        self.engineRpm = kwargs.get("engineRpm")
+        self.throttlePosition = kwargs.get("throttlePosition")
+        # TODO: think about other data (e.g. wheel speed)
+
---
+++ src/data/receiver.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from data.data import DataPoint
+from utils.config import Config, unit
+
+class DataReceiver(ABC):
+    def __init__(self, config: Config):
+        self.config = config
+
+    @abstractmethod
+    def isNewDataAvailable(self) -> bool:
+        pass
+
+    @abstractmethod
+    def getData(self) -> DataPoint:
+        pass
+
+
+class StubDataReceiver(DataReceiver):
+    def isNewDataAvailable(self) -> bool:
+        return True
+
+    def getData(self) -> DataPoint:
+        return DataPoint(
+            coolantTemp=97.3 * unit.degC,
+            oilPressure=60 * unit.psi,
+            batteryVoltage=12.3 * unit.volt,
+            lambdaValue=14.5,
+            engineRpm=12500 * unit.rpm,
+            throttlePosition=0.83,
+        )
+
+
+class SerialDataReceiver(DataReceiver):
+    def isNewDataAvailable(self) -> bool:
+        ...
+
+    def getData(self) -> DataPoint:
+        ...
